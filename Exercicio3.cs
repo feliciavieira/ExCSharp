926f6a69dc8e9d2dc8f91961376128e91e327254
Felícia Souza <146648130+feliciavieira@users.noreply.github.com>
2025-03-22 21:21:28 -0300

    Exercicio3.cs

---
+++ Exercicio3.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a + b + c);
+
+        }
+    }
+}
